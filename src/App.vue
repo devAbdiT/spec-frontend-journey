eb2ea6775a3ebde180b278a8ad12e4d88f538d18
Abdi begna <abditadesse89@gmail.com>
2026-02-04 21:22:48 +0300

    Conditional Greeting App

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,28 @@
-<!-- <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script> -->
-
-<!-- Topic One -->
-<!-- <script setup>
-const name = "SPEC Developer";
-</script>
-
-<template>
-  <h1>Hello {{ name }}</h1>
-</template> -->
-
-<!-- Topic TWo  -->
 <script setup>
 import { ref } from "vue";
+
 const name = ref("");
 </script>
 
 <template>
   <input v-model="name" placeholder="Type your name" />
-  <p>Hello {{ name }}</p>
-  <ul>
-    <li v-for="lang in ['JS', 'Python', 'Go']" :key="lang">
-      {{ lang }}
-    </li>
-  </ul>
+  <p v-if="name">Hello {{ name }}!</p>
 </template>
+
 <style scoped>
-header {
-  line-height: 1.5;
+input {
+  padding: 10px;
+  font-size: 16px;
+  margin-bottom: 20px;
+  width: 200px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+p {
+  font-size: 18px;
+  color: #42b883;
+  font-weight: bold;
+  margin-top: 10px;
 }
 </style>
